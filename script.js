b7507a9f6620c909135032ec8d712553b032fb0a
mahiWritesLogic <rajmahi4225@gmail.com>
2025-06-06 18:56:04 +0530

    v0.1

---
+++ script.js
@@ -0,0 +1,7 @@
+// Listen for add note button
+document.getElementById('addNoteBtn').addEventListener('click', () => {
+  const noteText = document.getElementById('noteInput').value;
+  // Save note to localStorage or backend
+  // Update UI
+  // Show notification
+});
